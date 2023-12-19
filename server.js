688314d71f12b7e63227b5ce98c0063f33971ce5
Churchill <churchilldaniel687@gmail.com>
2023-12-20 00:03:07 +0100

    got the server working and added some files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 //routes
@@ -6,6 +7,21 @@ app.get("/", (req, res) => {
   res.send("Hello node api");
 });
 
-app.listen(3002, () => {
-  console.log("listenting to port 3002");
+app.post("/product", (req, res) => {
+  console.log(req.body);
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://churchillrimamskep:11235813@cluster0.kutqosa.mongodb.net/node=API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3002, () => {
+      console.log("listenting to port 3002");
+    });
+
+    console.log("connected to mongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
